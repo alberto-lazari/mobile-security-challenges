666d0cdeb538bd6cb6aab4136b1dbe30ce18f26c
Alberto Lazari <alberto.lazari@gmail.com>
2023-10-08 17:53:43 +0200

    I may have the hash

--- 01-filehasher/MaliciousApp/app/src/main/java/com/example/maliciousapp/HashFile.java
+++ 01-filehasher/MaliciousApp/app/src/main/java/com/example/maliciousapp/HashFile.java
@@ -6,24 +6,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 import org.apache.commons.codec.digest.DigestUtils;
 import java.io.FileInputStream;
+import android.content.Context;
+import android.net.Uri;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import android.content.ContentResolver;
 
 public class HashFile extends AppCompatActivity {
-    private static String calcHash(final String filePath) {
-        FileInputStream fileInputStream = null;
-        try {
-            fileInputStream = new FileInputStream(filePath);
-            return DigestUtils.sha256Hex(fileInputStream);
-        } catch (Exception e) {
-            return e.getMessage();
-        } finally {
-            if (fileInputStream != null) {
-                try {
-                    fileInputStream.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+    public static FileInputStream getFileInputStreamFromContentUri(Context context, Uri contentUri) throws IOException {
+        ContentResolver contentResolver = context.getContentResolver();
+        InputStream inputStream = contentResolver.openInputStream(contentUri);
+        return inputStream instanceof FileInputStream ? (FileInputStream) inputStream : null;
     }
 
     @Override
@@ -31,15 +25,18 @@ public class HashFile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hash_file);
 
-        final TextView text = findViewById(R.id.text);
-
         final Intent intent = getIntent();
-        final String filePath = intent.getData()
-                                      .toString()
-                                      .replaceAll("^content:/", "/data");
-        final String hash = calcHash(filePath);
 
-        text.setText("File: " + filePath + "\nHash: " + hash);
+        String hash = null;
+        try {
+            final FileInputStream fileInputStream = getFileInputStreamFromContentUri(this, intent.getData());
+            hash = DigestUtils.sha256Hex(fileInputStream);
+        } catch (Exception e) {
+            hash = e.getMessage();
+        }
+
+        final TextView text = findViewById(R.id.text);
+        text.setText("Hash: " + hash);
     }
 }
 
