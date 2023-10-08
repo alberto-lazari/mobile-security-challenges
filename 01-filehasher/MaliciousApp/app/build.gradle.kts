658b5d82ad5590f829f41db9d373b12f8295dc1a
Alberto Lazari <alberto.lazari@gmail.com>
2023-10-08 11:16:05 +0200

    File is not accessible

--- 01-filehasher/MaliciousApp/app/build.gradle.kts
+++ 01-filehasher/MaliciousApp/app/build.gradle.kts
@@ -29,4 +29,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("commons-codec:commons-codec:1.15")
 }
--- 01-filehasher/MaliciousApp/app/src/main/java/com/example/maliciousapp/HashFile.java
+++ 01-filehasher/MaliciousApp/app/src/main/java/com/example/maliciousapp/HashFile.java
@@ -4,17 +4,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
+import org.apache.commons.codec.digest.DigestUtils;
+import java.io.FileInputStream;
 
 public class HashFile extends AppCompatActivity {
+    private static String calcHash(final String filePath) {
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(filePath);
+            return DigestUtils.sha256Hex(fileInputStream);
+        } catch (Exception e) {
+            return e.getMessage();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hash_file);
 
-        TextView text = findViewById(R.id.text);
+        final TextView text = findViewById(R.id.text);
 
-        Intent intent = getIntent();
-        text.setText("I received: " + intent.getData());
+        final Intent intent = getIntent();
+        final String filePath = intent.getData()
+                                      .toString()
+                                      .replaceAll("^content:/", "/data");
+        final String hash = calcHash(filePath);
+
+        text.setText("File: " + filePath + "\nHash: " + hash);
     }
 }
 
