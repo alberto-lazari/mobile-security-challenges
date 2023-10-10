cd0d9a44e60bf2e17f1293e7ac981d85b6b8bdd4
Alberto Lazari <alberto.lazari@gmail.com>
2023-10-10 08:51:39 +0200

    More idiomatic code

--- 01-filehasher/MaliciousApp/app/src/main/java/com/example/maliciousapp/HashFile.kt
+++ 01-filehasher/MaliciousApp/app/src/main/java/com/example/maliciousapp/HashFile.kt
@@ -1,40 +1,27 @@
 package com.example.maliciousapp
 
-import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
 import android.widget.TextView
 import org.apache.commons.codec.digest.DigestUtils
-import java.io.InputStream
-import java.io.FileInputStream
-import java.io.IOException
-import android.content.Context
-import android.net.Uri
 import android.app.Activity
 
 class HashFile : AppCompatActivity() {
-    @Throws(IOException::class)
-    fun getFileInputStream(context: Context, contentUri: Uri): FileInputStream? {
-        val inputStream: InputStream? = context.contentResolver.openInputStream(contentUri)
-        return if (inputStream is FileInputStream) inputStream else null
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hash_file)
 
-        var hash: String? = null
-        try {
-            val fileInputStream = getFileInputStream(this, intent.data!!)
-            hash = DigestUtils.sha256Hex(fileInputStream)
-        } catch (e: Exception) {
-            hash = e.message
+        val fileUri = intent.data
+        if (fileUri != null) {
+            val fileInputStream = contentResolver.openInputStream(fileUri)
+            val hash = DigestUtils.sha256Hex(fileInputStream)
+
+            // Print hash on the app for debug
+            findViewById<TextView>(R.id.text).text = "Hash: $hash"
+
+            setResult(Activity.RESULT_OK, Intent().putExtra("hash", hash))
+            finish()
         }
-
-        // Print hash on the app for debug
-        findViewById<TextView>(R.id.text).text = "Hash: $hash"
-
-        setResult(Activity.RESULT_OK, Intent().putExtra("hash", hash))
-        finish()
     }
 }
