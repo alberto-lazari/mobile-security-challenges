e37afb35e5a3f2fcc383ecaa0c0fde5a8ed086e3
Alberto Lazari <alberto.lazari@gmail.com>
2023-10-25 13:15:50 +0200

    Reduce latency

--- 04-whereareyou/whereareyou_checker.py
+++ 04-whereareyou/whereareyou_checker.py
@@ -21,7 +21,7 @@ def print_logs():
     f = open("whereareyou_logs.txt", "w")
     subp.call(["adb", "logcat", "-c"])
     try:
-        subp.call(["adb", "logcat", "-s", "MOBIOTSEC"], stdout=f, timeout=3)
+        subp.call(["adb", "logcat", "-s", "MOBIOTSEC"], stdout=f, timeout=1)
     except subp.TimeoutExpired:
         parse_logs()
 
