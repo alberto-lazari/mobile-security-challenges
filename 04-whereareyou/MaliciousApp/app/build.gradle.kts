89276f743833c04f3cc95cca95f7e08555dd460e
Alberto Lazari <alberto.lazari@gmail.com>
2023-10-26 11:59:30 +0200

    It works

--- 04-whereareyou/MaliciousApp/app/build.gradle.kts
+++ 04-whereareyou/MaliciousApp/app/build.gradle.kts
@@ -34,4 +34,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
